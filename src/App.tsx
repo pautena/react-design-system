9d84c4a12368f62c1d90580c14d2f7628e3587d1
Pau Tena Vidal <pau.tena@socialpoint.es>
2022-05-20 13:12:11 +0200

    Add theme and remove css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {
   BrowserRouter,
 } from "react-router-dom";
-import './app.css'
 import { RootNavigator } from './navigators/root-navigator';
+import {ThemeProvider,createTheme,CssBaseline} from '@mui/material';
 import {Auth} from 'aws-amplify';
 import { CognitoAuthConfig } from './configs/aws';
 import { Provider } from "react-redux";
@@ -11,13 +11,18 @@ import { store } from './store';
 
 Auth.configure(CognitoAuthConfig);
 
+const theme = createTheme();
+
 function App() {
   return (
     <Provider store={store}>
-    <BrowserRouter>
-      <RootNavigator/>
-    </BrowserRouter>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <RootNavigator/>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
