832bfe141def728e5c66e011421971da7fe04446
Pau Tena Vidal <pau.tena@socialpoint.es>
2022-05-20 09:52:28 +0200

    Install material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
+import {Box,Typography} from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Typography variant="h1">Shapers toolbox</Typography>
+    </Box>
   );
 }
 
