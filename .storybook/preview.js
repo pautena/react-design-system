a358d4b34738f0c95685df9c552be6c3a0aea186
Pau Tena <me@protonmail.com>
2022-06-29 20:37:13 +0200

    Add theme selector under Knobs tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,36 @@
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 import { addDecorator } from "@storybook/react";
 import { Box } from "@mui/material";
+import { select,withKnobs } from "@storybook/addon-knobs";
 
-addDecorator((Story) => (
-  <ThemeProvider theme={createTheme()}>
-    <CssBaseline />
-    <Box px={2} py={3}>
-      <Story />
-    </Box>
-  </ThemeProvider>
-));
+const light = createTheme();
+const dark = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
+const themes = { light, dark };
+const themeNames = Object.keys(themes);
+
+addDecorator((Story) => {
+  const theme = select(
+    "Theme",
+    themeNames,
+    themeNames[0],
+    "Themes"
+  );
+
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <CssBaseline />
+      <Box px={2} py={3}>
+        <Story />
+      </Box>
+    </ThemeProvider>
+  )
+});
+addDecorator(withKnobs)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
