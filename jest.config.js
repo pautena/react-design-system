b0aeb08d2c0c0bef729cc53f08740678e06a39a0
Pau Tena Vidal <pau.tena@socialpoint.es>
2022-07-15 10:13:27 +0200

    Setup coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  collectCoverageFrom: [
+    "./src/**",
+    '!src/**/*.stories.tsx',
+    '!src/storybook.ts',
+    '!src/tests/**'
+  ],
 };
