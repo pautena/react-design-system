488da5f5fdedd966a72ca2835d856747e48634ac
Pau Tena Vidal <pau.tena@socialpoint.es>
2022-08-20 09:42:07 +0200

    Remove tab-layout

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,6 +1,5 @@
 export * from "./data-table-layout";
 export * from "./depr-header-layout";
-export * from "./tab-layout";
 export * from "./list-layout";
 export * from "./app-bar-with-drawer-layout";
 export * from "./details-layout";
