d740d864eda17c083b21826781d85150a4ae083e
Pau Tena <me@pautena.com>
2023-04-16 10:25:34 +0200

    Test DrawerAppBar

--- src/components/drawers/drawer-app-bar/drawer-app-bar.test.tsx
+++ src/components/drawers/drawer-app-bar/drawer-app-bar.test.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { render, screen } from "~/tests/testing-library";
 import { DrawerAppBar } from "./drawer-app-bar";
 import { DrawerProvider } from "../drawer-provider";
-import { DrawerState } from "~/components/drawers";
+import { DrawerState, DrawerVariant } from "~/components/drawers";
 import { Typography } from "@mui/material";
 
 describe("DrawerAppBar", () => {
   const renderComponent = ({
     title,
     initialState,
+    variant = "temporary",
   }: {
     title?: string;
     initialState?: DrawerState;
+    variant?: DrawerVariant;
   } = {}) => {
     render(
-      <DrawerProvider initialState={initialState}>
+      <DrawerProvider initialState={initialState} variant={variant}>
         <DrawerAppBar title={title}>
           <Typography>test content</Typography>
         </DrawerAppBar>
@@ -22,10 +24,25 @@ describe("DrawerAppBar", () => {
     );
   };
 
-  it("should render a menu button", () => {
-    renderComponent();
+  it.each([
+    ["temporary" as DrawerVariant, "open" as DrawerState],
+    ["temporary" as DrawerVariant, "close" as DrawerState],
+    ["persistent" as DrawerVariant, "open" as DrawerState],
+    ["persistent" as DrawerVariant, "close" as DrawerState],
+    ["mini" as DrawerVariant, "collapse" as DrawerState],
+  ])("should render a menu button if variant is %s", (variant, initialState) => {
+    renderComponent({ variant, initialState });
 
-    expect(screen.getByRole("button", { name: /open drawer/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /open drawer/i })).toBeVisible();
+  });
+
+  it.each([
+    ["clipped" as DrawerVariant, "open" as DrawerState],
+    ["mini" as DrawerVariant, "open" as DrawerState],
+  ])("should not render a menu button if variant is %s", (variant, initialState) => {
+    renderComponent({ variant, initialState });
+
+    expect(screen.queryByRole("button", { name: /open drawer/i })).not.toBeInTheDocument();
   });
 
   it("should render the children", () => {
