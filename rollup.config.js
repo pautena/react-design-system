d4ce17d8f9c1a4dea9f488bfe31a25318da826d2
Pau Tena Vidal <pau.tena@socialpoint.es>
2023-09-12 16:55:33 +0200

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -82,7 +82,7 @@ export default [
     external: ["react", "react-dom"],
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
