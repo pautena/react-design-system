40b217d798e9b0797948eab45b8cc4b391b95e4e
Pau Tena <me@protonmail.com>
2025-02-25 22:06:54 +0100

    Fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,20 @@ import { peerDependencies } from "./package.json";
 export default defineConfig({
   build: {
     lib: {
-      entry: "./src/index.ts", // Specifies the entry point for building the library.
-      name: "vite-react-ts-button", // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      external: [...Object.keys(peerDependencies)],
     },
-    sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
+    sourcemap: true,
+    emptyOutDir: true,
   },
-  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [
+    dts({
+      tsconfigPath: "./tsconfig.build.json",
+    }),
+  ],
 });
