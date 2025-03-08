d5a72ef4e71d2ed9f98c5b9580794cb9858cdda5
Pau Tena Vidal <me@pautena.com>
2025-03-08 23:54:11 +0100

    Refactor and restructure storybook (#321)
    * Recreate storybook
    
    * reformat basic documentation
    
    * Add a basic components documentation
    
    ---------
    
    Co-authored-by: Pau Tena <me@protonmail.com>

--- src/action/action.tsx
+++ src/action/action.tsx
@@ -11,21 +11,75 @@ import { useDialog } from "../dialog";
 export type ActionVariant = "primary" | "error" | "warning" | "success";
 
 export interface ActionProps {
+  /**
+   * The variant of the action.
+   */
   variant: ActionVariant;
+
+  /**
+   * The title of the action.
+   */
   title: string;
+
+  /**
+   * The variant of the title.
+   */
   titleVariant?: Extract<Variant, "h4" | "h5" | "h6">;
+
+  /**
+   * The description of the action.
+   */
   description?: string | ReactElement;
+
+  /**
+   * The variant of the description.
+   */
   descriptionVariant?: Variant;
+
+  /**
+   * The helper text for the action.
+   */
   helperText?: string;
+
+  /**
+   * The variant of the helper text.
+   */
   helperTextVariant?: Variant;
+
+  /**
+   * The text for the action button.
+   */
   buttonText: string;
+
+  /**
+   * Indicates if the action requires confirmation.
+   */
   confirmable?: boolean;
+
+  /**
+   * The title for the confirmation dialog.
+   */
   confirmTitle?: string;
+
+  /**
+   * The description for the confirmation dialog.
+   */
   confirmDescription?: string;
+
+  /**
+   * The passphrase required to perform action.
+   */
   passphrase?: string;
+
+  /**
+   * The callback function to be executed when the action is triggered.
+   */
   onAction: () => void;
 }
 
+/**
+ * Renders an action button with a text that explains what it does.
+ */
 export const Action = ({
   variant = "primary",
   title,
