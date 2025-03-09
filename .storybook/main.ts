cbf718e06573f040bfe8621ec952e9c4c930871c
Pau Tena <me@protonmail.com>
2025-03-09 10:28:08 +0100

    Refactor generators storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,8 @@ const config: StorybookConfig = {
   stories: [
     "../src/stories/introduction.mdx",
     "../src/**/*.mdx",
-     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-    ],
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
--- src/object-details/object-details.tsx
+++ src/object-details/object-details.tsx
@@ -121,6 +121,10 @@ export interface ObjectDetailsProps<T extends BasicModelInstance> {
   instance: T;
 }
 
+/**
+ * ObjectDetails component renders detailed information about a given instance of a model.
+ * It supports rendering various types of fields including single values, groups, and arrays of groups.
+ */
 export const ObjectDetails = <T extends BasicModelInstance>({
   model,
   instance,
