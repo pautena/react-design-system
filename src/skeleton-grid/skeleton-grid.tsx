ed82863eefd3c4ce612f99b23d00442331401d4f
Pau Tena <me@protonmail.com>
2025-03-09 21:45:42 +0100

    Document placeholders

--- src/skeleton-grid/skeleton-grid.tsx
+++ src/skeleton-grid/skeleton-grid.tsx
@@ -2,10 +2,22 @@ import Grid from "@mui/material/Grid";
 import { newArrayWithSize } from "../utils";
 import { SkeletonCard } from "../skeleton-card";
 
+/**
+ * Props for the SkeletonGrid component.
+ */
+export interface SkeletonGridProps {
+  /**
+   * Optional size of the skeleton grid.
+   */
+  size?: number;
+}
 export interface SkeletonGridProps {
   size?: number;
 }
 
+/**
+ * SkeletonGrid component renders a grid of skeleton cards.
+ */
 export const SkeletonGrid = ({ size = 20 }: SkeletonGridProps) => {
   return (
     <Grid container spacing={2}>
