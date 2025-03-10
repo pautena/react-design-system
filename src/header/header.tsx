2aa94d47f9533a7dfb3414fe00ac63561d0020c1
Pau Tena <me@protonmail.com>
2025-03-10 20:42:09 +0100

    Change default tabsMode to navigation

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -25,7 +25,7 @@ export const Header = ({
   breadcrumbs,
   actions,
   tabs,
-  tabsMode = "panel",
+  tabsMode = "navigation",
   navigationButton,
 }: HeaderProps) => {
   const { palette } = useTheme();
